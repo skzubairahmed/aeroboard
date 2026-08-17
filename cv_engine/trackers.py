2a09d5663ae56e92fc94453554afac696085669f
Sk Zubair Ahmed <sheikhzubairahmed3949@gmail.com>
2026-08-17 23:28:38 +0530

    Restructuring

--- cv_engine/trackers.py
+++ cv_engine/trackers.py
@@ -4,5 +4,23 @@ class Tracker:
     def __init__(self, camera_idx:int = 0):
         self.camera_idx = camera_idx
         self.frame = None
+        self.cap = cv2.VideoCapture(camera_idx, )
 
-    self.
+    def showFrames(self): #for demo/tests
+        window_name = "Original Frame"
+        cv2.namedWindow(window_name)
+
+        try:
+            ret, self.frame = self.cap.read()
+
+            if not ret:
+                return "Unable to read camera."
+
+            self.frame = cv2.flip(self.frame, 1)
+            key = (cv2.waitKey(1) & 0xFF)
+
+            cv2.imshow()
+
+            
+
+        
